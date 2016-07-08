ca308a5a74db5f11b3563ca3050f3a8716def96c
koudaiii <cs006061@gmail.com>
2016-07-08 20:08:54 +0900

    Fix config root_controller

--- controller/root_controller.go
+++ controller/root_controller.go
@@ -4,13 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/koudaiii/kong-oauth-token-generator/config"
 )
 
-func Index(c *gin.Context) {
+type RootController struct {
+	*ApplicationController
+}
+
+func NewRootController(config *config.KongConfiguration) *RootController {
+	return &RootController{NewApplicationController(config)}
+}
+
+func (s *RootController) Index(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"alert":     false,
 		"error":     false,
 		"logged_in": true,
 		"message":   "",
 	})
+	return
 }

e424a4bb105a001ad7989ed771f0fa3a5d475d24
koudaiii <cs006061@gmail.com>
2016-07-08 16:46:38 +0900

    Add Create/Update/Delete method

--- kong/oauth2.go
+++ kong/oauth2.go
@@ -26,19 +26,29 @@ type OAuth2Config struct {
 // Services
 
 // ConfigService provides methods for creating and reading issues.
-type Oauth2Service struct {
+type OAuth2Service struct {
 	sling *sling.Sling
 }
 
-// NewCOauth2Service returns a new Oauth2Service.
-func NewOauth2Service(httpClient *http.Client, config *config.KongConfiguration) *Oauth2Service {
-	return &Oauth2Service{
+// NewCOAuth2Service returns a new OAuth2Service.
+func NewOAuth2Service(httpClient *http.Client, config *config.KongConfiguration) *OAuth2Service {
+	return &OAuth2Service{
 		sling: sling.New().Client(httpClient).Base(config.KongAdminURL + "consumers/"),
 	}
 }
 
-func (s *Oauth2Service) Get(consumerName string, oauth2ID string) (Oauth2Config, *http.Response, error) {
-	oauth2config := new(Oauth2Config)
+func (s *OAuth2Service) Create(params *OAuth2Config, consumerName string) (*OAuth2Config, *http.Response, error) {
+	oauth2 := new(OAuth2Config)
+	kongError := new(KongError)
+	resp, err := s.sling.New().Post("http://localhost:8001/consumers/"+consumerName+"/oauth2").BodyJSON(params).Receive(oauth2, kongError)
+	if err == nil {
+		err = kongError
+	}
+	return oauth2, resp, err
+}
+
+func (s *OAuth2Service) Get(consumerName string, oauth2ID string) (OAuth2Config, *http.Response, error) {
+	oauth2config := new(OAuth2Config)
 	kongError := new(KongError)
 	resp, err := s.sling.New().Path(consumerName+"/oauth2/"+oauth2ID).Receive(oauth2config, kongError)
 	if err == nil {
@@ -47,12 +57,32 @@ func (s *Oauth2Service) Get(consumerName string, oauth2ID string) (Oauth2Config,
 	return *oauth2config, resp, err
 }
 
-func (s *Oauth2Service) List(consumerName string) (Oauth2List, *http.Response, error) {
-	oauth2 := new(Oauth2List)
+func (s *OAuth2Service) List(consumerName string) (OAuth2List, *http.Response, error) {
+	oauth2 := new(OAuth2List)
 	kongError := new(KongError)
 	resp, err := s.sling.New().Path(consumerName+"/oauth2").Receive(oauth2, kongError)
 	if err == nil {
 		err = kongError
 	}
 	return *oauth2, resp, err
+}
+
+func (s *OAuth2Service) Update(params *OAuth2Config, consumerName string) (*OAuth2Config, *http.Response, error) {
+	oauth2 := new(OAuth2Config)
+	kongError := new(KongError)
+	resp, err := s.sling.New().Patch("http://localhost:8001/consumers/"+consumerName+"/oauth2/"+params.ID).BodyJSON(params).Receive(oauth2, kongError)
+	if err == nil {
+		err = kongError
+	}
+	return oauth2, resp, err
+}
+
+func (s *OAuth2Service) Delete(oauth2ID string, consumerName string) (string, *http.Response, error) {
+	var message string
+	kongError := new(KongError)
+	resp, err := s.sling.New().Delete("http://localhost:8001/consumers/"+consumerName+"/oauth2/"+oauth2ID).Receive(message, kongError)
+	if err == nil {
+		err = kongError
+	}
+	return message, resp, err
 }

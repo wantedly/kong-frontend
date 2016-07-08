a0530cd0e4ddda46aa55b71a6f076579c97e13e8
koudaiii <cs006061@gmail.com>
2016-07-08 14:47:51 +0900

    ADD test code

--- kong/kong.go
+++ kong/kong.go
@@ -44,17 +44,17 @@ func main() {
 
 	client := NewClient(nil, config)
 
-	apis, _, _ := client.APIService.List()
-	fmt.Printf("APIs:\n%v\n", apis)
+	//	apis, _, _ := client.APIService.List()
+	//	fmt.Printf("APIs:\n%v\n", apis)
+	//
+	//	api, _, _ := client.APIService.Get("go-api")
+	//	fmt.Printf("API:\n%v\n", api)
 
-	api, _, _ := client.APIService.Get("go-api")
-	fmt.Printf("API:\n%v\n", api)
+	//	plugins, _, _ := client.PluginService.List("go-api")
+	//	fmt.Printf("Plugins:\n%v\n", plugins)
 
-	plugins, _, _ := client.PluginService.List("go-api")
-	fmt.Printf("Plugins:\n%v\n", plugins)
-
-	plugin, _, _ := client.PluginService.Get("go-api", "ec04cf37-920d-46fd-adb5-ce33f416d88b")
-	fmt.Printf("Plugin:\n%v\n", plugin)
+	//	plugin, _, _ := client.PluginService.Get("go-api", "ec04cf37-920d-46fd-adb5-ce33f416d88b")
+	//	fmt.Printf("Plugin:\n%v\n", plugin)
 
 	consumers, _, _ := client.ConsumerService.List()
 	fmt.Printf("Consumers:\n%v\n", consumers)
@@ -62,18 +62,51 @@ func main() {
 	consumer, _, _ := client.ConsumerService.Get("gokun")
 	fmt.Printf("Consumer:\n%v\n", consumer)
 
-	oauth2, _, _ := client.Oauth2Service.List("gokun")
-	fmt.Printf("Oauth2:\n%v\n", oauth2)
-
-	targetOauth2Config, _, _ := client.Oauth2Service.Get("gokun", "86e4f18c-00a6-403f-a526-5d8fc3dac95d")
-	fmt.Printf("Oauth2:\n%v\n", targetOauth2Config)
-
-	generateAPI := &API{
-		Name:             "sakabe",
-		UpstreamURL:      "http://koudaiii.com",
-		RequestHost:      "test.com",
-		StripRequestPath: true,
+	generateConsumer := &Consumer{
+		Username: "sakabe",
 	}
-	api, resp, err := client.APIService.Create(generateAPI)
-	fmt.Printf(":\n%v\n%v\n%v\n%v\n", generateAPI, api, resp, err)
+
+	consumer, resp, err := client.ConsumerService.Create(generateConsumer)
+	fmt.Printf("Create Consumer :\n%v\n%v\n%v\n%v\n", generateConsumer, &consumer, resp, err)
+
+	updateConsumer := &Consumer{
+		ID:       consumer.ID,
+		Username: "sakabeupdate",
+	}
+
+	updatedConsumer, resp, err := client.ConsumerService.Update(updateConsumer)
+	fmt.Printf("Update Consumer :\n%v\n%v\n%v\n%v\n", updateConsumer, &updatedConsumer, resp, err)
+
+	message, resp, err := client.ConsumerService.Delete(updatedConsumer.ID)
+	fmt.Printf("Delete Consumer : \n%v\n%v\n%v\n", message, resp, err)
+
+	//	oauth2, _, _ := client.Oauth2Service.List("gokun")
+	//	fmt.Printf("Oauth2:\n%v\n", oauth2)
+
+	//	targetOauth2Config, _, _ := client.Oauth2Service.Get("gokun", "86e4f18c-00a6-403f-a526-5d8fc3dac95d")
+	//	fmt.Printf("Oauth2:\n%v\n", targetOauth2Config)
+
+	//	generateAPI := &API{
+	//		Name:             "sakabe",
+	//		UpstreamURL:      "http://koudaiii.com",
+	//		RequestHost:      "test.com",
+	//		StripRequestPath: true,
+	//	}
+	//	api, resp, err := client.APIService.Create(generateAPI)
+	//	fmt.Printf("Create API :\n%v\n%v\n%v\n%v\n", generateAPI, &api, resp, err)
+
+	//	updateAPI := &API{
+	//		ID:               api.ID,
+	//		Name:             "sakabeupdate",
+	//		UpstreamURL:      "http://koudaiii.com",
+	//		RequestHost:      "test.com",
+	//		StripRequestPath: true,
+	//	}
+
+	//	updatedAPI, resp, err := client.APIService.Update(updateAPI)
+	//	fmt.Printf("Update API :\n%v\n%v\n%v\n%v\n", updateAPI, &updatedAPI, resp, err)
+
+	//	message, resp, err := client.APIService.Delete(updatedAPI.ID)
+	//	fmt.Printf("Delete API : \n%v\n%v\n%v\n", message, resp, err)
+
 }

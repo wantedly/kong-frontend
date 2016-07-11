b85f4063bcf414efb5ae8303e804fe2f0f9e93d0
koudaiii <cs006061@gmail.com>
2016-07-11 16:28:10 +0900

    Fix server

--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/koudaiii/kong-oauth-token-generator/config"
 	"github.com/koudaiii/kong-oauth-token-generator/controller"
+	"github.com/koudaiii/kong-oauth-token-generator/kong"
 )
 
 func Run(config *config.KongConfiguration) {
@@ -11,9 +12,14 @@ func Run(config *config.KongConfiguration) {
 	r.Static("/assets", "assets")
 	r.LoadHTMLGlob("templates/*")
 
-	rootController := controller.NewRootController(config)
+	client := kong.NewClient(nil, config)
+	rootController := controller.NewRootController(client)
+	apiController := controller.NewAPIController(client)
+	// consumerController := controller.NewConsumerController(config)
+	// oauth2Controller := controller.NewOAuth2Controller(config)
 
 	r.GET("/", rootController.Index)
+	r.GET("/api", apiController.Index)
 
 	r.Run()
 }

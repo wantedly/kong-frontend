f8f952596c81e0029778da3da694ab74b0e5318b
koudaiii <cs006061@gmail.com>
2016-07-11 16:28:44 +0900

    Add template consumers

--- model/consumer/consumer.go
+++ model/consumer/consumer.go
@@ -1,1 +1,14 @@
 package consumer
+
+import (
+	"fmt"
+
+	"github.com/koudaiii/kong-oauth-token-generator/config"
+	"github.com/koudaiii/kong-oauth-token-generator/kong"
+)
+
+func List(config *config.KongConfiguration) (*kong.Consumers, error) {
+	client := kong.NewClient(nil, config)
+	consumers, _, err := client.ConsumerService.List()
+	return consumers, err
+}

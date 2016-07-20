f3026fbf9d951db7a67f60086e12b635f5c726d5
koudaiii <cs006061@gmail.com>
2016-07-20 18:05:13 +0900

    Fix if

--- model/api/api.go
+++ model/api/api.go
@@ -25,7 +25,7 @@ func List(self *kong.Client) (*kong.APIs, error) {
 
 func Exists(self *kong.Client, apiName string) bool {
 	_, resp, _ := self.APIService.Get(apiName)
-	if resp != nil {
+	if resp == nil {
 		return false
 	}
 	if resp.StatusCode != 404 {

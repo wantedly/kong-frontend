f83d92106b66dd81ef4b5dcccf02d2df96813441
koudaiii <cs006061@gmail.com>
2016-07-21 16:47:38 +0900

    Delete root

--- server/server.go
+++ server/server.go
@@ -14,13 +14,11 @@ func Run(config *config.KongConfiguration) {
 
 	r.Use(gin.Logger())
 	client := kong.NewClient(nil, config)
-	rootController := controller.NewRootController(client)
 	apiController := controller.NewAPIController(client)
 	// consumerController := controller.NewConsumerController(config)
 	// oauth2Controller := controller.NewOAuth2Controller(config)
 
-	r.GET("/", rootController.Index)
-
+	r.GET("/", apiController.Index)
 	r.GET("/apis", apiController.Index)
 	r.POST("/apis", apiController.Create)
 	r.GET("/apis/:apiName", apiController.Get)

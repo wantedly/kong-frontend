f4efbcff6909c5eb98c72b54599fb9dfeb40fbda
koudaiii <cs006061@gmail.com>
2016-07-21 19:09:56 +0900

    Exists and Get and Create in OAuth2

--- model/oauth2/oauth2.go
+++ model/oauth2/oauth2.go
@@ -12,3 +12,30 @@ func List(self *kong.Client) (*kong.Consumers, *kong.AssigneesOAuth2s, error) {
 
 	return consumers, assignees, err
 }
+
+func Exists(self *kong.Client, consumerName string) bool {
+	_, resp, _ := self.ConsumerService.Get(consumerName)
+	if resp == nil {
+		return false
+	}
+	if resp.StatusCode != 404 {
+		return true
+	}
+	return false
+}
+
+func Get(self *kong.Client, consumerName string) (*kong.Consumer, error) {
+	consumer, _, err := self.ConsumerService.Get(consumerName)
+	if err != nil {
+		return nil, err
+	}
+	return consumer, err
+}
+
+func Create(self *kong.Client, generateConsumer *kong.Consumer) (*kong.Consumer, error) {
+	consumer, _, err := self.ConsumerService.Create(generateConsumer)
+	if err != nil {
+		return nil, err
+	}
+	return consumer, err
+}

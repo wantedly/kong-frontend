9b4a2f9d711bcfd4e38cd95caf76ba4fcdf10b35
bgpat <bgpat23@gmail.com>
2016-08-25 12:07:20 +0900

    Add tag for forms

--- kong/oauth2_plugin.go
+++ kong/oauth2_plugin.go
@@ -1,13 +1,13 @@
 package kong
 
 type OAuth2PluginConfig struct {
-	AcceptHTTPIfAlreadyTerminated bool   `json:"accept_http_if_already_terminated,omitempty"`
-	EnableAuthorizationCode       bool   `json:"enable_authorization_code,omitempty"`
-	EnableClientCredentials       bool   `json:"enable_client_credentials,omitempty"`
-	EnableImplicitGrant           bool   `json:"enable_implicit_grant,omitempty"`
-	EnablePasswordGrant           bool   `json:"enable_password_grant,omitempty"`
-	HideCredentials               bool   `json:"hide_credentials,omitempty"`
-	MandatoryScope                bool   `json:"mandatory_scope,omitempty"`
-	ProvisionKey                  string `json:"provision_key,omitempty"`
-	TokenExpiration               int    `json:"token_expiration,omitempty"`
+	AcceptHTTPIfAlreadyTerminated bool   `form:"accept_http_if_already_terminated" json:"accept_http_if_already_terminated,omitempty"`
+	EnableAuthorizationCode       bool   `form:"enable_authorization_code" json:"enable_authorization_code,omitempty"`
+	EnableClientCredentials       bool   `form:"enable_client_credentials" json:"enable_client_credentials,omitempty"`
+	EnableImplicitGrant           bool   `form:"enable_implicit_grant" json:"enable_implicit_grant,omitempty"`
+	EnablePasswordGrant           bool   `form:"enable_password_grant" json:"enable_password_grant,omitempty"`
+	HideCredentials               bool   `form:"hide_credentials" json:"hide_credentials,omitempty"`
+	MandatoryScope                bool   `form:"mandatory_scope" json:"mandatory_scope,omitempty"`
+	ProvisionKey                  string `form:"provision_key" json:"provision_key,omitempty"`
+	TokenExpiration               int    `form:"token_expiration" json:"token_expiration,omitempty"`
 }

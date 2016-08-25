6113d21b117d5cf65d8fadafb4fb369504b9c938
bgpat <bgpat23@gmail.com>
2016-08-25 12:09:52 +0900

    Add route to config plugin

--- server/server.go
+++ server/server.go
@@ -37,6 +37,7 @@ func Run(config *config.KongConfiguration) {
 	r.GET("/apis/:apiName/plugins/:pluginID", pluginController.Get)
 	r.POST("/apis/:apiName/plugins/:pluginID/delete", pluginController.Delete)
 	r.GET("/apis/:apiName/new-plugin", pluginController.New)
+	r.POST("/apis/:apiName/new-plugin", pluginController.Config)
 
 	r.Run()
 }
